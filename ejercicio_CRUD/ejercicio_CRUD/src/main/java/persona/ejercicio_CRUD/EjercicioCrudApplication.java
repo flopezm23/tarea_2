6636dafb7c6386ebcaebd3190ee1769c11afea39
flopezm23 <92342303+flopezm23@users.noreply.github.com>
2024-08-15 23:28:58 -0600

    Add files via upload

---
+++ ejercicio_CRUD/ejercicio_CRUD/src/main/java/persona/ejercicio_CRUD/EjercicioCrudApplication.java
@@ -0,0 +1,13 @@
+package persona.ejercicio_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioCrudApplication.class, args);
+	}
+
+}
